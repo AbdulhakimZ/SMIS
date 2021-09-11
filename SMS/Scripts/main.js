6afa7f3ddc57ffccdb63e99618bd8dccec07fdc2
Abdulhakim Zeinu <zluckyza@gmail.com>
2021-09-11 15:43:07 +0300

    Scripts

---
+++ SMS/Scripts/main.js
@@ -0,0 +1,31 @@
+﻿
+function RenderActions(RenderActionstring) {
+    $("#OpenDialog").load(RenderActionstring);
+};
+
+function RenderEditActions(RenderActionstring) {
+    $("#EditOpenDialog").load(RenderActionstring);
+};
+function RenderDeleteActions(RenderActionstring) {
+    $("#DeleteOpenDialog").load(RenderActionstring);
+};
+function Clean() {
+    $('#modalCreate').find('textarea,input').val('');
+};
+
+
+function ValidateInputs() {
+    var flag = true;
+    var nameInput = $('#Name');
+
+    if ($.trim(nameInput.val()) != '') {
+        nameInput.closest('.form-group').removeClass('has-error');
+        flag = true;
+    }
+    if ($.trim(nameInput.val()) === '') {
+        nameInput.closest('.form-group').addClass('has-error');
+  
+        flag = false;
+    }
+    return flag;
+};
